a97f7cbacbff272cece7886d5de48306736f2cbc
NovaNo1r <mail@novanoir.dev>
2022-03-15 21:27:05 +0800

    Fixed bug

--- src/plugins/BUPT_ncov/__init__.py
+++ src/plugins/BUPT_ncov/__init__.py
@@ -13,6 +13,11 @@ driver = get_driver()
 plugin_config = Config.parse_obj(get_driver().config)
 scheduler: AsyncIOScheduler = require("nonebot_plugin_apscheduler").scheduler
 
+if not Path.exists(plugin_config.data_path):
+    Path.mkdir(plugin_config.data_path, parents=True)
+if not Path.exists(plugin_config.secret_path):
+    Path.mkdir(plugin_config.secret_path, parents=True)
+
 users = []
 checkin_times = set()
 xisu_checkin_times = set()
@@ -255,11 +260,6 @@ async def scheduler_checkin(time: str, type_: int = 0):
 @driver.on_bot_connect
 async def _():
     bot = get_bot()
-    # Initialize dir
-    if not Path.exists(plugin_config.data_path):
-        Path.mkdir(plugin_config.data_path, parents=True)
-    if not Path.exists(plugin_config.secret_path):
-        Path.mkdir(plugin_config.secret_path, parents=True)
 
     user = BUPTUser(0)
 
