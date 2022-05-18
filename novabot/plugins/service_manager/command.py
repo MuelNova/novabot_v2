1271d2346585a175b348bc24ad321c5a3395e281
NovaNo1r <mail@novanoir.dev>
2022-05-18 12:04:14 +0800

    :sparkles: Add service_manager v1

--- novabot/plugins/service_manager/command.py
+++ novabot/plugins/service_manager/command.py
@@ -1,5 +1,8 @@
 from nonebot import on_command
-from nonebot.adapters.onebot.v11 import GroupMessageEvent
+from nonebot.permission import SUPERUSER
+from nonebot.params import CommandArg, Arg
+from nonebot.typing import T_State
+from nonebot.adapters.onebot.v11 import GroupMessageEvent, GROUP_OWNER, GROUP_ADMIN, Message
 
 from .data_source import GlobalVar as gV
 from .rule import is_able_in_group
@@ -9,17 +12,69 @@ __all__ = [
     "get_service_list"
 ]
 
-get_service_list = on_command("lssm", priority=1)
-get_service_list = Service("service_manager", get_service_list)
+get_service_list = on_command("lssv",
+                              aliases={'服务列表', '功能列表'},
+                              priority=1,
+                              permission=GROUP_OWNER | GROUP_ADMIN | SUPERUSER)
+
+set_enable = on_command("enable",
+                        aliases={'启用', '开启', '打开'},
+                        priority=1,
+                        permission=GROUP_OWNER | GROUP_ADMIN | SUPERUSER)
+set_disable = on_command('disable',
+                         aliases={'禁用', '关闭'},
+                         priority=1,
+                         permission=GROUP_OWNER | GROUP_ADMIN | SUPERUSER)
 
 
 @get_service_list.handle()
-async def _(event: GroupMessageEvent):
+async def _(event: GroupMessageEvent, all_: Message = CommandArg()):
     msg = f"{event.group_id}的服务开启情况:\n"
     for name_, services in gV.service_bundle.items():
         msg += f"  {name_}:\n"
         for service in services:
             msg += ("    |" +
                     ("√" if is_able_in_group(service, event) else "×") +
-                    f"| {service.plugin_name}\n") if service.visible else ""
+                    f"| {service.plugin_name}\n") if service.visible or all_ else ""
     await get_service_list.finish(msg)
+
+
+@set_enable.handle()
+@set_disable.handle()
+async def _(state: T_State, service: Message = CommandArg()):
+    if service:
+        state['service'] = service
+
+
+@set_enable.got("service", prompt="你想启用什么服务呢？")
+async def _(event: GroupMessageEvent,
+            service_name: Message = Arg("service")):
+    service_name = service_name.extract_plain_text()
+    enabled_services = []
+    if service_name.lower() in ['all', '全部']:
+        for service in Service.get_loaded_services().values():
+            service.set_enable(event.group_id)
+        enabled_services = ["All"]
+    else:
+        for service in service_name.split(" "):
+            if sv := Service.get_loaded_services().get(service):
+                sv.set_enable(event.group_id)
+                enabled_services.append(service)
+    await set_enable.finish("已启用服务: " + ",".join(enabled_services))
+
+
+@set_disable.got("service", prompt="你想禁用什么服务呢？")
+async def _(event: GroupMessageEvent,
+            service_name: Message = Arg("service")):
+    service_name = service_name.extract_plain_text()
+    disabled_services = []
+    if service_name.lower() in ['all', '全部']:
+        for service in Service.get_loaded_services().values():
+            service.set_disable(event.group_id)
+        disabled_services = ["All"]
+    else:
+        for service in service_name.split(" "):
+            if sv := Service.get_loaded_services().get(service):
+                sv.set_disable(event.group_id)
+                disabled_services.append(service)
+    await set_disable.finish("已禁用服务: " + ",".join(disabled_services))
