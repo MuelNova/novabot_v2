6a320948d6ad49da9ee481b7193f9935771f8bb7
NovaNo1r <mail@novanoir.dev>
2022-05-18 15:15:53 +0800

    :construction: Add service_manager with CD and daily limit.

--- novabot/plugins/service_manager/rule.py
+++ novabot/plugins/service_manager/rule.py
@@ -1,6 +1,11 @@
 from abc import abstractmethod
+from datetime import datetime
+from typing import Optional
 
-from nonebot.adapters.onebot.v11 import Event
+from nonebot.rule import Rule
+from nonebot.typing import T_State
+from nonebot.adapters.onebot.v11 import Event, Bot
+
 
 
 class ServiceRule:
@@ -11,7 +16,7 @@ class ServiceRule:
 
     @abstractmethod
     async def __call__(self, *args, **kwargs) -> bool:
-        ...
+        raise NotImplementedError
 
     def __repr__(self):
         return f"<Rule of {self.service.plugin_name}>"
@@ -20,6 +25,77 @@ class ServiceRule:
 class GroupRule(ServiceRule):
     async def __call__(self, event: Event) -> bool:
         return is_able_in_group(self.service, event)
+
+    def __repr__(self):
+        return f"<GroupRule of {self.service.plugin_name}>"
+
+
+class OnlyRule(ServiceRule):
+    @abstractmethod
+    async def __call__(self, *args, **kwargs) -> bool:
+        raise NotImplementedError
+
+    async def is_correct_event(self, bot: Bot, event: Event, state: T_State) -> bool:
+        """
+        Improvable.
+
+        There'll have 2*n-1 rules to be checked.
+        Absolutely a waste.
+        """
+        rule = Rule()
+        for checker in self.service.rule.checkers:
+            if isinstance(checker.call, OnlyRule):
+                continue
+            rule &= checker
+        return await rule(bot, event, state)
+
+
+class CDRule(OnlyRule):
+
+    def __init__(self, service: "Service", reply: Optional[str] = None):
+        super().__init__(service)
+        self.service = service
+        self.reply = reply
+
+    async def __call__(self, event: Event, bot: Bot, state: T_State) -> bool:
+        result = await self.is_correct_event(bot, event, state)
+        if not result:
+            return False
+        if hasattr(event, 'group_id') and hasattr(event, 'user_id'):
+            ts = datetime.now().timestamp()
+            if ts < (cd_ts := self.service.get_cd(event.group_id, event.user_id)) and cd_ts != 0:
+                if self.reply:
+                    await bot.send(event, self.reply)
+                return False
+            self.service.set_cd(event.group_id, event.user_id, ts+self.service.cd)
+        return True
+
+    def __repr__(self):
+        return f"<CD Rule of {self.service.plugin_name}>"
+
+
+class LimitRule(OnlyRule):
+
+    def __init__(self, service: "Service", reply: Optional[str] = None):
+        super().__init__(service)
+        self.service = service
+        self.reply = reply
+
+    async def __call__(self, event: Event, bot: Bot, state: T_State) -> bool:
+        result = await self.is_correct_event(bot, event, state)
+        if not result:
+            return False
+        if hasattr(event, 'group_id') and hasattr(event, 'user_id'):
+            count = self.service.get_limit(event.group_id, event.user_id)
+            if count >= self.service.limit != 0:
+                if self.reply:
+                    await bot.send(event, self.reply)
+                return False
+            self.service.set_limit(event.group_id, event.user_id, count+1)
+        return True
+
+    def __repr__(self):
+        return f"<Limit Rule of {self.service.plugin_name}>"
 
 
 def is_able_in_group(service: "Service", event: Event) -> bool:
--- novabot/plugins/service_manager/service.py
+++ novabot/plugins/service_manager/service.py
@@ -1,25 +1,32 @@
 import json
+from datetime import date
 from pathlib import Path
-from typing import Type, Dict, Any, Optional, Set
+from typing import Type, Dict, Any, Optional, Set, Union
 
 from nonebot.internal.matcher import Matcher, MatcherMeta
 from nonebot.log import logger
 
 from .data_source import GlobalVar as gV
-from .rule import GroupRule
+from .rule import GroupRule, CDRule, LimitRule
 from ...utils import _load_file, _save_file
 
 
 class Service:
     plugin_name: str
     matcher: Type[Matcher]
-    use_priv: Optional[int]
-    manage_priv: Optional[int]
-    enable_on_default: Optional[bool]
-    visible: Optional[bool]
-    help: Optional[str]
+    use_priv: int
+    manage_priv: int
+    enable_on_default: bool
+    visible: bool
+    help: str
     enable_group: Set[int]
     disable_group: Set[int]
+    cd: int
+    limit: int
+    cd_reply: Optional[str]
+    limit_reply: Optional[str]
+    cd_list: Optional[Dict[str, Dict[str, Union[float, int]]]]
+    cd_list: Optional[Dict[str, Union[Dict[str, Union[float, int]], str]]]
 
     def __init__(self,
                  plugin_name: str,
@@ -29,7 +36,11 @@ class Service:
                  enable_on_default=None,
                  visible: Optional[bool] = None,
                  help_: Optional[str] = None,
-                 bundle: Optional[str] = None):
+                 bundle: Optional[str] = None,
+                 cd: Optional[int] = None,
+                 limit: Optional[int] = None,
+                 cd_reply: Optional[str] = None,
+                 limit_reply: Optional[str] = None):
         """
         将一个matcher进行包装，成为可控制的Service对象
 
@@ -37,11 +48,17 @@ class Service:
         :param matcher: 需要包装的matcher
         :param use_priv: 使用权限，默认值: 所有成员
         :param manage_priv: 管理权限，默认值：群管理
-        :param enable_on_default: 默认启用，默认值: True
+        :param enable_on_default: 是否默认启用，默认值: True
         :param visible: 是否可见，默认值: True
         :param help_: 帮助文档，默认值: None
         :param bundle: 插件分类，默认值: '默认'
+        :param cd: 插件调用CD，单位为秒，默认值: 0
+        :param limit: 插件调用限制，单位为次，0为无限制，默认值: 0
+        :param cd_reply: 插件冷却时回复，空为不回复，默认值: 空
+        :param limit_reply: 插件调用限制时回复，空为不回复，默认值: 空
+
         """
+
         if not isinstance(matcher, MatcherMeta):
             raise TypeError(f"matcher excepted yet {type(matcher)} found.")
         self.plugin_name = plugin_name
@@ -55,6 +72,16 @@ class Service:
         self.use_priv = config.get('use_priv') or use_priv or 0
         self.manage_priv = config.get('manage_priv') or manage_priv or 0
         # End To-Do
+        self.help = help_
+        self.enable_group = set(config.get('enable_group', []))
+        self.disable_group = set(config.get('disable_group', []))
+        self.cd = config.get('cd') or cd or 0
+        self.limit = config.get('limit') or limit or 0
+        self.cd_reply = config.get('cd_reply') or cd_reply or ''
+        self.cd_list = config.get('cd_list', {})
+        self.limit_reply = config.get('limit_reply') or limit_reply or ''
+        self.limit_list = config.get('limit_list', {"date": str(date.today())})
+
         self.enable_on_default = config.get('enable_on_default')
         if self.enable_on_default is None:
             self.enable_on_default = enable_on_default
@@ -65,11 +92,12 @@ class Service:
             self.visible = visible
         if self.visible is None:
             self.visible = True
-        self.help = help_
-        self.enable_group = set(config.get('enable_group', []))
-        self.disable_group = set(config.get('disable_group', []))
 
         self.matcher.rule &= GroupRule(self)
+        if self.cd > 0:
+            self.matcher.rule &= CDRule(self, self.cd_reply)
+        if self.limit > 0:
+            self.matcher.rule &= LimitRule(self, self.limit_reply)
 
         gV.loaded_service[self.plugin_name] = self
         gV.service_bundle[bundle or '默认'].append(self)
@@ -79,7 +107,9 @@ class Service:
                f" use_priv={self.use_priv}," \
                f" manage_priv={self.manage_priv}," \
                f" enable_on_default={self.enable_on_default}," \
-               f" visible={self.visible}>"
+               f" visible={self.visible}," \
+               f" cd={self.cd}," \
+               f" limit={self.limit}>"
 
     def __getattr__(self, item):
         return getattr(self.matcher, item)
@@ -102,7 +132,13 @@ class Service:
                        "enable_on_default": self.enable_on_default,
                        "visible": self.visible,
                        "enable_group": list(self.enable_group),
-                       "disable_group": list(self.disable_group)
+                       "disable_group": list(self.disable_group),
+                       "cd": self.cd,
+                       "cd_reply": self.cd_reply,
+                       "cd_list": self.cd_list,
+                       "limit": self.limit,
+                       "limit_reply": self.limit_reply,
+                       "limit_list": self.limit_list
                    })
 
     def set_enable(self, group_id: int):
@@ -120,3 +156,23 @@ class Service:
     @staticmethod
     def get_loaded_services() -> Dict[str, "Service"]:
         return gV.loaded_service
+
+    def get_cd(self, group_id: int, user_id: int):
+        return self.cd_list.get(str(group_id), {}).get(str(user_id), 0)
+
+    def set_cd(self, group_id: int, user_id: int, timestamp: float):
+        group_cd_list = self.cd_list.get(str(group_id), {})
+        group_cd_list.update({str(user_id): timestamp})
+        self.cd_list[str(group_id)] = group_cd_list
+        self._save_config()
+
+    def get_limit(self, group_id: int, user_id: int):
+        if self.limit_list.get('date', '') != str(date.today()):
+            self.limit_list = {"date": str(date.today())}
+        return self.limit_list.get(str(group_id), {}).get(str(user_id), 0)
+
+    def set_limit(self, group_id: int, user_id: int, counts: int):
+        group_limit_list = self.limit_list.get(str(group_id), {})
+        group_limit_list.update({str(user_id): counts})
+        self.limit_list[str(group_id)] = group_limit_list
+        self._save_config()
