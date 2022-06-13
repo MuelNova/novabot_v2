96b5a9bef2cf702fe0bdbb25f8118014b789e002
NovaNo1r <mail@novanoir.dev>
2022-06-13 22:48:27 +0800

    :sparkles: Add GroupManager plugin

--- novabot/helpers.py
+++ novabot/helpers.py
@@ -1,4 +1,4 @@
-from nonebot.adapters.onebot.v11 import GroupMessageEvent
+from nonebot.adapters.onebot.v11 import GroupMessageEvent, Bot, Event
 from nonebot.matcher import Matcher
 from nonebot.params import Depends
 
@@ -19,3 +19,13 @@ def is_reply_event() -> None:
         await matcher.finish()
 
     return Depends(_is_reply_event)
+
+
+def is_admin_or_owner() -> None:
+    async def _is_admin_or_owner(matcher: Matcher, event: Event, bot: Bot):
+        # sourcery skip: merge-nested-ifs
+        if hasattr(event, 'group_id'):
+            if (await bot.call_api("get_group_member_info", group_id=event.group_id, user_id=event.self_id)).\
+                    get('role') in ['owner', 'admin']:
+                return
+        await matcher.finish()
