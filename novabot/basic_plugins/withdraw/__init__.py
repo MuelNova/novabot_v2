2843e696bb520a0307be2a26b0a5799f28c156a6
NovaNo1r <mail@novanoir.dev>
2022-06-13 22:51:52 +0800

    :construction: Add Service for withdraw plugin

--- novabot/basic_plugins/withdraw/__init__.py
+++ novabot/basic_plugins/withdraw/__init__.py
@@ -4,10 +4,14 @@ from nonebot.adapters.onebot.v11.exception import ActionFailed
 from nonebot.adapters.onebot.v11.permission import GROUP
 from nonebot.permission import SUPERUSER
 
+from novabot.basic_plugins.service_manager import Service
+
 from ...helpers import is_reply_my_msg, is_reply_event
 
 with_draw = on_command(".withdraw", aliases={"撤回", "撤"}, permission=GROUP)
 s_with_draw = on_command(".withdraw", aliases={"撤回", "撤"}, permission=SUPERUSER)
+with_draw = Service("回复撤回", with_draw)
+s_with_draw = Service("SU回复撤回", s_with_draw, visible=False)
 
 
 @with_draw.handle([is_reply_my_msg()])
