71f0a62ae4a3592457e5901f04ccafbf987c9391
NovaNo1r <mail@novanoir.dev>
2022-06-28 11:11:51 +0800

    :sparkles: Add VoteMute service

--- novabot/basic_plugins/group_manager/vote_mute.py
+++ novabot/basic_plugins/group_manager/vote_mute.py
@@ -50,7 +50,7 @@ async def _(bot: Bot, event: GroupMessageEvent):
         await vote_to_mute.send(f"哼哼哼，好好带上吧，这是{get_nickname()}的奖励!")
         user_mute = set()
     else:
-        await vote_to_mute.send(f"哦呀呀，已经有{len(user_mute)}想要给{MessageSegment.at(usr_id)}"
+        await vote_to_mute.send(f"哦呀呀，已经有{len(user_mute)}想要给" + MessageSegment.at(usr_id) +
                                 f"带上口球啦，集齐3票的话{get_nickname()}就要过来啦OpO")
 
     mute_dict[str(usr_id)] = user_mute
